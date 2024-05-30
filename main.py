cce973c885f209169ac38507575ec95b25029475
xsismadn3ss <2477carlos.artiga@gmail.com>
2024-05-29 22:44:41 -0600

    change theme

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import flet as ft
-from flet import Column, Row, Text, colors
+from flet import colors
 from web3 import Web3
 from dotenv import load_dotenv
+from controls.Tittle import Tittle
+from controls.Textfield import TextFieldCustom
+from controls.View import View
+from controls.Button import CustomButton
 import os
 
 # load_dotenv()
@@ -11,56 +15,47 @@ import os
 # w3 = Web3(Web3.HTTPProvider(infura_api))
 # testing connection
 # print(w3.is_connected())
+user = "Test"
+password = "hello"
 
 
 def main(page: ft.Page):
-    page.title = "PRESCRYPTO"
-    page.window_height = 800
-    page.window_width = 500
+    page.title = "Prescrytpo"
     page.padding = 18
 
-    def check_balance(e):
-        try:
-            wallet = wallet_token.value
-            # balance=w3.eth.get_balance(wallet)
-            dlg = ft.AlertDialog(
-                title=ft.Text(f"\nAvailable balance:\n{balance}ETH\n"),
-                on_dismiss=lambda e: print("Dialog dissmissed"),
-            )
-        except Exception as e:
-            dlg = ft.AlertDialog(
-                title=ft.Text(
-                    f"\nIncorrrect token\n\n{e}\n",
-                ),
-                on_dismiss=lambda e: print("Dialog dissmissed"),
-            )
-        wallet_token.value = ""
-        page.dialog = dlg
-        dlg.open = True
-        page.update()
+    # def check_balance(e):
+    #     try:
+    #         wallet = wallet_token.value
+    #         # balance=w3.eth.get_balance(wallet)
+    #         dlg = ft.AlertDialog(
+    #             title=ft.Text(f"\nAvailable balance:\n{balance}ETH\n"),
+    #             on_dismiss=lambda e: print("Dialog dissmissed"),
+    #         )
+    #     except Exception as e:
+    #         dlg = ft.AlertDialog(
+    #             title=ft.Text(
+    #                 f"\nIncorrrect token\n\n{e}\n",
+    #             ),
+    #             on_dismiss=lambda e: print("Dialog dissmissed"),
+    #         )
+    #     wallet_token.value = ""
+    #     page.dialog = dlg
+    #     dlg.open = True
+    #     page.update()
 
-    wallet_token = ft.TextField(
-        label="Enter your token wallet",
-        border_color=colors.PINK_900,
-        border_radius=10,
-        focused_border_color=colors.PINK_400,
-        # bgcolor=colors.GREY_900,
-        # hover_color=colors.BLACK12,
-    )
-    balance = Column(
+    # VIEWS
+    sign_in = View(
         controls=[
-            Text(value="Balance", size=45),
-            wallet_token,
-            ft.ElevatedButton(
-                text="Check your balance",
-                bgcolor=colors.PINK_800,
-                color=colors.WHITE,
-                # on_click=check_balance
-            ),
-        ]
+            Tittle("Sign In"),
+            TextFieldCustom("Enter your token wallet"),
+            TextFieldCustom("Paste your private key", True),
+            TextFieldCustom("Set a pin", True),
+        ],
     )
-    explore = Column(controls=[Text(value="Explore", size=45)])
-    account = Column(controls=[Text(value="Account", size=45)])
+
+    balance = View(controls=[Tittle("Balance"), CustomButton("Check balance")])
+    explore = View(controls=[Tittle("Explore")])
+    account = View(controls=[Tittle("Account")])
 
     def change_route(e):
         index = e.control.selected_index
@@ -100,7 +95,7 @@ def main(page: ft.Page):
         ),
     ]
     page.navigation_bar = ft.NavigationBar(
-        indicator_color=colors.PINK_800,
+        indicator_color=colors.GREEN_800,
         destinations=[
             section[0],
             section[1],
@@ -110,7 +105,7 @@ def main(page: ft.Page):
     )
 
     page.add(
-        # inicio
+        # pagina de inicio
         balance
     )
 
